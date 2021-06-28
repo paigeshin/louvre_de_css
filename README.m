0d9b0e73e87c2802622645a4801a7133e4bd4b8b
innertainment <innertainment@innertainmentui-MacBookPro.local>
2021-06-28 19:22:39 +0900

    complete

---
+++ README.m
@@ -0,0 +1,157 @@
+# louvre_de_web
+
+# CSS Checklist
+
+- [ ]  Remove Browser Inconsistency
+- [ ]  import font from css
+- [ ]  body margin zero
+- [ ]  margin collasping
+- [ ]  box-sizing: border-box
+- [ ]  Implement `normalize.css` to eliminate browser's inconsistency
+- [ ]  Define commonly used variables
+- [ ]  Using, [https://autoprefixer.github.io](https://autoprefixer.github.io/) , for multiple browser support
+- [ ]  Using @Support query for multiple browser
+- [ ]  Whether or not using `polyfills` ⇒ [https://polyfill.io/v3/](https://polyfill.io/v3/) (support unsupported features for older browsers)
+- [ ]  CSS Naming using BEM (Block__Element—Modifier)
+
+### background image
+
+- [ ]  background-position: center
+- [ ]  background-size: cover
+- [ ]  background-origin: border-box
+- [ ]  background-clip: border-box
+
+
+
+# CSS Lecture by Max
+[Section 1: Getting Started ](https://www.notion.so/Section-1-Getting-Started-b61843a19d9949f084ca53c29d1d1e6e)
+
+[Section 2, Diving Into the Basics of CSS](https://www.notion.so/Section-2-Diving-Into-the-Basics-of-CSS-5c6d29095cec42b68901b1b441f29426)
+
+[How to import Google fonts](https://www.notion.so/How-to-import-Google-fonts-5e980a3d42764c38a819320cb975066a)
+
+[Section 3: Diving Deeper into CSS](https://www.notion.so/Section-3-Diving-Deeper-into-CSS-08216d43925a4d3f97c5b54ceef6af86)
+
+[Section 4: More on Selectors & CSS Features](https://www.notion.so/Section-4-More-on-Selectors-CSS-Features-979f41ea0c2a42719edbcc2c8aa16dab)
+
+[Section 5, Practicing the Basics](https://www.notion.so/Section-5-Practicing-the-Basics-2de5af2cd44043a184bde988977dbbe9)
+
+[Section 6, Positioning](https://www.notion.so/Section-6-Positioning-fe0685d239b04af2a9690f5d9853bd96)
+
+[Section 7,  Understanding Background Images ](https://www.notion.so/Section-7-Understanding-Background-Images-5b375ac347de4cc4b292101e7dc57765)
+
+[Handle image tag](https://www.notion.so/Handle-image-tag-c57117ed4e0e4b519335b8021c1645ee)
+
+[Fix inline-element bug, annoying white space ](https://www.notion.so/Fix-inline-element-bug-annoying-white-space-21424d944e4646968b1876be756fb4cb)
+
+[Linear Gradient](https://www.notion.so/Linear-Gradient-b45e228b90464520ac8c83fb43d0d311)
+
+[Radial Gradient](https://www.notion.so/Radial-Gradient-f5a6c2901b51400a948c4bb658c2bafd)
+
+[background overlay magic formula](https://www.notion.so/background-overlay-magic-formula-7a68767c3c4b4a0ea0633e12b77c537c)
+
+[Understanding Filters](https://www.notion.so/Understanding-Filters-638f1a04676945b48b85703ca354380e)
+
+[Adding SVGs](https://www.notion.so/Adding-SVGs-c9a8f33b5e5645ff80933af3b7b33e65)
+
+[Section 8, Sizes & Units](https://www.notion.so/Section-8-Sizes-Units-f31569e312b0410db65778b8095a3b8f)
+
+[Using auto to center element](https://www.notion.so/Using-auto-to-center-element-65095b10dc654c588de11a294d244bd6)
+
+[Section 9 , working with javscript & css ](https://www.notion.so/Section-9-working-with-javscript-css-9988a65715434b629c51158d366095eb)
+
+[Modal ](https://www.notion.so/Modal-b0cbb19a8325405189f26b86db6841c6)
+
+[Backdrop](https://www.notion.so/Backdrop-28ba3dcde7464f5aa6b0a0a2e9a4e1df)
+
+[Section 10, Making our Website Responsive](https://www.notion.so/Section-10-Making-our-Website-Responsive-7332c85241504189b70e2d2bad17cf77)
+
+[Meta tag for mobile device optimization](https://www.notion.so/Meta-tag-for-mobile-device-optimization-c1d1c7198f9448ff8e37c1a1339faaf2)
+
+[Meta tag options](https://www.notion.so/Meta-tag-options-c7767ae180e34a2180216c0a00151442)
+
+[Media Query](https://www.notion.so/Media-Query-9bb58042eac349f399ef7c449c979fdd)
+
+[Position footer correctly](https://www.notion.so/Position-footer-correctly-dd2caa9b93d8468f9f3780d93123ff3b)
+
+[Section 11, Styling Form Elements](https://www.notion.so/Section-11-Styling-Form-Elements-219265a16577429fbd0c304ac1c09386)
+
+[Advanced Attributed Selectors](https://www.notion.so/Advanced-Attributed-Selectors-79b8b66b47114cfb9a92c3996114d6a0)
+
+[Form Styling Example](https://www.notion.so/Form-Styling-Example-535886394c5340f2904b3cd4b040a78b)
+
+[Section 12. Text & Fonts](https://www.notion.so/Section-12-Text-Fonts-0e59d9ab61834caa80ea4c9cb71753f7)
+
+[Import web font from google](https://www.notion.so/Import-web-font-from-google-be7623f9f7974c308f887739b662d6f9)
+
+[Understanding font-family syntax](https://www.notion.so/Understanding-font-family-syntax-9bbaca113bee4c059701efd190bc05a5)
+
+[Import web fonts only on a specific css file](https://www.notion.so/Import-web-fonts-only-on-a-specific-css-file-63eee8f140fc478f87d0a65e2641603d)
+
+[Importing custom font](https://www.notion.so/Importing-custom-font-2a59ccbfe9a54e388cde2fe8e26313f6)
+
+[Diving into font properties](https://www.notion.so/Diving-into-font-properties-455cbe5105ae4f70a8d0d88f74f1dbf6)
+
+[List of essential font properties](https://www.notion.so/List-of-essential-font-properties-c361a847849043a8a1136e4e41c1828c)
+
+[Library of Flexbox ](https://www.notion.so/Library-of-Flexbox-abf4807b413f4cd38e8c01689b73e964)
+
+[CSS Float & Clear](https://www.notion.so/CSS-Float-Clear-d472674aca5d49839ba5f3fa4aed1bc3)
+
+[Section 14, CSS Grid](https://www.notion.so/Section-14-CSS-Grid-b89ea91b6ebe4499b073167d000c9829)
+
+[**Grid Best Practices*****](https://www.notion.so/Grid-Best-Practices-227ae24e5200468b8303138835eb12fc)
+
+[Grid Shorthand](https://www.notion.so/Grid-Shorthand-1291599141bc45959bfba316bf9fe171)
+
+[Adding Named Template Areas***](https://www.notion.so/Adding-Named-Template-Areas-bd88c8ebbf75461b8d14fc81aaacc86d)
+
+[About Auto](https://www.notion.so/About-Auto-469c9f539dec42d6a3d0695036a035fa)
+
+[Set footer at the bottom using grid](https://www.notion.so/Set-footer-at-the-bottom-using-grid-cde85571f7ed4bf6af954bc782b1fe59)
+
+[Two ways to use grid*****](https://www.notion.so/Two-ways-to-use-grid-bb09814f09e6405ea9d92e86f7c5b50c)
+
+[All about grid positioning ***](https://www.notion.so/All-about-grid-positioning-707db04ada54456bbc1695da80b543c3)
+
+[Build Responsive Layout with Grid *****](https://www.notion.so/Build-Responsive-Layout-with-Grid-d8bf6ea0e15540e49904b68a7fac4679)
+
+[Grid Autoflow](https://www.notion.so/Grid-Autoflow-ca58b78935cb49c0bd042a7f8631f5e7)
+
+[Grid cell size properties, auto-fill, auto-fit](https://www.notion.so/Grid-cell-size-properties-auto-fill-auto-fit-b7fad12a8baa42d89e1e8f66e8af27b3)
+
+[Section 15, Transforming Elements with CSS Transforms](https://www.notion.so/Section-15-Transforming-Elements-with-CSS-Transforms-30cfb3b712a8422ba820e3489f0ddc33)
+
+[Section 16: Transitions & Animations in CSS](https://www.notion.so/Section-16-Transitions-Animations-in-CSS-5a0e0ce97c1442208a89397068150131)
+
+[Transition example](https://www.notion.so/Transition-example-3848ee9b66da4f73bd078911a552439a)
+
+[Modal Transition Magic Formula](https://www.notion.so/Modal-Transition-Magic-Formula-2b5a11ff2ad048159cf775cad8d2a4a6)
+
+[How to handle *display: block* with transition?](https://www.notion.so/How-to-handle-display-block-with-transition-91091fc53c5f425ea4ac4c24f22612d1)
+
+[CSS keyframes](https://www.notion.so/CSS-keyframes-6c12ccd1ddb9435597f3eda2ad0df06b)
+
+[Section 17: Writing Future-Proof CSS Code](https://www.notion.so/Section-17-Writing-Future-Proof-CSS-Code-82752436052d4d58be63e0db9c67d71c)
+
+[CSS Variable](https://www.notion.so/CSS-Variable-da01c41dfe714a2bb565e2a752d2f354)
+
+[Vendor Prefixes ](https://www.notion.so/Vendor-Prefixes-c515e297c92d4523b19efff166d12a97)
+
+[@supports query for multiple browser support](https://www.notion.so/supports-query-for-multiple-browser-support-ee0c7c152ce5478f9875ea4df1d600ef)
+
+[Polyfiils for multiple browser support](https://www.notion.so/Polyfiils-for-multiple-browser-support-df9e1d028e4b45d8a3ea71e960a9007c)
+
+[Eliminate Cross-Browser Inconsistencies ](https://www.notion.so/Eliminate-Cross-Browser-Inconsistencies-481ee23f1a8d4666880c9d43c0c9f702)
+
+[How to Name CSS Classes (BEM System) ](https://www.notion.so/How-to-Name-CSS-Classes-BEM-System-f101c9f221a44f71b24b8b272825b0b8)
+
+# SASS
+
+[Section 18, Introducing Sass(Syntactically Awesome Style Sheets)](https://www.notion.so/Section-18-Introducing-Sass-Syntactically-Awesome-Style-Sheets-941740791a104da191af7ab5622142e0)
+
+---
+
+[Summary of modules ](https://www.notion.so/Summary-of-modules-843aa723d4cf44eca6cc828b8f7a1f4c)
+
+[Checklist](https://www.notion.so/Checklist-e86597ae85174f068046fc619ddadf1b)
